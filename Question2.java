7e1a32d14f368c71670d38925d50361abf39536a
DannyPolyLife <64061976+DannyPolyLife@users.noreply.github.com>
2020-04-21 15:43:29 +0800

    Final

--- Question2.java
+++ Question2.java
@@ -4,13 +4,14 @@ public class Question2
 {
   public static void main(String[] args)
   {
-    /**
-     * Prompts user for height and weight, calculate and print out the height and weight of the user
-     * 
-     * Hint: in.nextDouble() to read double
-     */
-     
-    Scanner in = new Scanner(System.in);
-    double height = in.nextDouble();
+    Scanner InputOfUser = new Scanner(System.in);
+
+    System.out.printf("Enter height: ");
+    String height = InputOfUser.nextLine(); 
+    System.out.printf("Enter weight: ");
+    String weight = InputOfUser.nextLine(); 
+
+    System.out.println("Your height is " + height);
+    System.out.println("Your weight is " + weight);
   }
 }
