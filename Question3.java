7cb7a0c125c6461ba108211fb0e58723260948fa
DannyPolyLife <64061976+DannyPolyLife@users.noreply.github.com>
2020-04-22 12:18:27 +0800

    adding of comments

--- Question3.java
+++ Question3.java
@@ -5,10 +5,10 @@ public class Question3
   public static void main(String[] args)
   {
     Scanner InputofUser = new Scanner(System.in);
-
+    //asking for input
     System.out.printf("Enter an integer: ");
     int intInput = InputofUser.nextInt();
-
+    //calculation and displaying
     System.out.println("Your input Integer multiplied by themself is:  " + (intInput * intInput));
     
   }
