2fa669b6961ea40561be3d2c0427ef4094b41a96
DannyPolyLife <64061976+DannyPolyLife@users.noreply.github.com>
2020-04-22 12:20:19 +0800

    Adding of comments

--- Question4.java
+++ Question4.java
@@ -5,10 +5,11 @@ public class Question4
   public static void main(String[] args)
   {
     Scanner InputofUser = new Scanner(System.in);
-    
+    //asking of input
     System.out.printf("Enter right angle: ");
     int numberOfAsterick = InputOfUser.nextInt();
-
+    
+    //displaying of right angle based on input
     int fixedIntStart = numberOfAsterick;
     for(int i = 0; i < fixedIntStart; i++){
       for(int j = 0; j < numberOfAsterick; j++){
