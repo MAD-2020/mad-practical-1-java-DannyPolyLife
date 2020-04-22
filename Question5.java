29bdec5eae39eca87751f73e2701979f1d5ea15a
DannyPolyLife <64061976+DannyPolyLife@users.noreply.github.com>
2020-04-22 12:22:53 +0800

    Adding Comments

--- Question5.java
+++ Question5.java
@@ -6,23 +6,28 @@ public class Question5
   {
     Scanner InputOfUser = new Scanner(System.in);
     
+    //asking for number of integers
     System.out.printf("Enter number of integer you want to enter: ");
     int numberofIntegers = InputOfUser.nextInt();
 
     System.out.println("You have chosen to enter " + numberofIntegers + " integers");
-
+    
+    //Asking for integers, number of integers depend on the first input
     ArrayList<Integer> Integers = new ArrayList<>();
     for(int i = 0; i < numberofIntegers; i++){
       System.out.printf("Enter an integer: ");
       int dataInteger = InputOfUser.nextInt();
       Integers.add(dataInteger);
     }
+    
+    //finding the mode
     int highestValue = Integers.get(0);
     for(int i : Integers){
       if (i > highestValue){
         highestValue = i;
       }
     }
+    //displaying mode
     System.out.println("The mode of these integers is: "+ highestValue);
     
   }
